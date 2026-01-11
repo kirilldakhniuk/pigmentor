d595638b237aef017bf08c85b00615031e2869e4
Kirill Dakhniuk <cosmic@hey.com>
2026-01-11 11:26:23 +0200

    fix test in ci

--- resources/views/components/⚡color-history/color-history.test.php
+++ resources/views/components/⚡color-history/color-history.test.php
@@ -1,8 +1,13 @@
 <?php
 
 use Livewire\Livewire;
+use Native\Desktop\Facades\Settings;
 
 test('color can be removed from history', function () {
+    Settings::shouldReceive('get')
+        ->with('keep-history', config('app.keep_history'))
+        ->andReturn(config('app.keep_history'));
+
     $component = Livewire::test('color-history')
         ->call('setHistory', [$color = fake()->hexColor]);
 
